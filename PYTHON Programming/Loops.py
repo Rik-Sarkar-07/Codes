f46e7bc650550490bd3fa4a9a5a71690c7c18f7d
Sudipta Sarkar <84672557+Rik-Sarkar-07@users.noreply.github.com>
2021-09-19 13:27:34 +0530

    Create Loops.py

---
+++ PYTHON Programming/Loops.py
@@ -0,0 +1,9 @@
+# For loop 1 to 100   -- for(int i=1;i<101;i++)
+for i in range(1, 101):
+    print('Print = ', i)
+
+# While Loop 1 to 100
+i = 1
+while 101 > i:
+    print('Print = ', i)
+    i = i+1
